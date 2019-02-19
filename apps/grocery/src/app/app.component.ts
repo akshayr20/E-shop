5e49d778b31a0098e4492ef9c9f72444a35df4a6
akshayr20 <akshay.rajput5394@gmail.com>
2019-02-19 17:12:41 +0530

    added basic reset (styles) to the app

--- apps/grocery/src/app/app.component.ts
+++ apps/grocery/src/app/app.component.ts
@@ -5,23 +5,12 @@ import { User } from '@e-shop/data-models';
 @Component({
   selector: 'e-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
   title = 'grocery';
 
-  constructor(private _userService: UserService) {}
+  constructor() {}
 
-  ngOnInit() {
-    const user: User = {
-      email: 'akshay.rajput1@gmail.com',
-      password: '123456'
-    };
-    // this._userService.signUp(user).subscribe(res => {
-    //   console.log('res', res);
-    // });
-    this._userService.login(user).subscribe(res => {
-      console.log(res);
-    });
-  }
+  ngOnInit() {}
 }
