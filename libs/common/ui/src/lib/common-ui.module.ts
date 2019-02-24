d2fa84a884c3bb904acc4c8efe8d1f10746f30e8
akshayr20 <akshay.rajput5394@gmail.com>
2019-02-24 10:57:13 +0530

    code refactoring

--- libs/common/ui/src/lib/common-ui.module.ts
+++ libs/common/ui/src/lib/common-ui.module.ts
@@ -8,19 +8,24 @@ import { FooterComponent } from './footer/footer.component';
 import { RouterModule } from '@angular/router';
 import { ProductCardComponent } from './product-card/product-card.component';
 import { ProductQuantityComponent } from './product-quantity/product-quantity.component';
-import { SearchBarComponent } from './search-bar/search-bar.component';
-import { CategoriesComponent } from './categories/categories.component';
+import { NavbarComponent } from './header/navbar/navbar.component';
+import { SearchBarComponent } from './header/search/search-bar/search-bar.component';
+import { CategoriesComponent } from './header/search/categories/categories.component';
+import { SearchComponent } from './header/search/search.component';
+
 
 @NgModule({
   imports: [CommonModule, RouterModule, CommonDirectivesModule],
   declarations: [
     IconComponent,
     HeaderComponent,
+    NavbarComponent,
+    SearchBarComponent,
+    CategoriesComponent,
+    SearchComponent,
     FooterComponent,
     ProductCardComponent,
-    ProductQuantityComponent,
-    SearchBarComponent,
-    CategoriesComponent
+    ProductQuantityComponent
   ],
   exports: [
     IconComponent,
---
+++ libs/common/ui/src/lib/header/navbar/navbar.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ui-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent implements OnInit {
+  showMyAccountsDropDown = false;
+
+  constructor() {}
+
+  ngOnInit() {}
+}
